17768742c217ca9dbc2272a6d9d49cb15124cfed
gehanisharadj <145694949+gehanisharadj@users.noreply.github.com>
2023-09-21 21:12:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94743037153', 'me', true],
+  ['94743037153', 'DJ Gehan', true], 
   [''] 
 ] //Number of owners
 
